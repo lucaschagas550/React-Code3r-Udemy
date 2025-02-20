45b27d70e6f2d58929b393ef496bc1af1f664f21
Lucas <lucasandrade595@gmail.com>
2025-02-20 19:35:09 -0300

    first commit

---
+++ React comandos.cs
@@ -0,0 +1,18 @@
+//Baixa as dependencias somente local, sem ser algo global
+npx create-raect-app [nome do projeto]
+
+//Inicia o Projeto
+npm start
+
+npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
