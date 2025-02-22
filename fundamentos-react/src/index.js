9fb065228b56902f077f6aaf42cf6fb685120dfe
Lucas <lucasandrade595@gmail.com>
2025-02-21 23:15:27 -0300

    feat: index js e index css

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import './index.css';
+import React from 'react'; //JSX Precisa deste import
 import ReactDOM from 'react-dom/client';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-root.render(<h1>Olá, React!</h1>);
+root.render
+(
+    <h1>Olá, React!</h1>
+);
