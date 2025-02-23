0520902bae7f816da95cdc50c1fff8fee1a4b134
Lucas <lucasandrade595@gmail.com>
2025-02-23 11:30:14 -0300

    feat: card estilos como parametros

--- 01 - fundamentos-react/src/App.jsx
+++ 01 - fundamentos-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"; //JSX Precisa deste import
-import './App.css';
+import "./App.css";
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
@@ -9,28 +9,26 @@ import Card from "./layout/Card";
 
 export default () => (
   <div className="App">
-    
     <h1>Fundamento de React</h1>
 
-    <div className="Cards"> 
-      <Card titulo="#04 - Exemplo de Card">
-        <NumeroAleatorio numeroMinimo={1} numeroMaximo={10}/>
-        <NumeroAleatorio numeroMinimo={1} numeroMaximo={10}/>
+    <div className="Cards">
+      <Card titulo="#04 - Exemplo de Card" color="#090">
+        <NumeroAleatorio numeroMinimo={1} numeroMaximo={10} />
+        <NumeroAleatorio numeroMinimo={1} numeroMaximo={10} />
       </Card>
 
-      <Card titulo="#03 - Fragmento">
+      <Card titulo="#03 - Fragmento" color="#FA6900">
         <Fragmento></Fragmento>
       </Card>
 
-      <Card titulo="#02 - Com parametro">
-        <ComParametro titulo='Aluno' subtitulo='Teste' nota={8}/>
-        <ComParametro titulo='Aluno' subtitulo='Teste' nota={8}/>
+      <Card titulo="#02 - Com parametro" color="#E94C6F">
+        <ComParametro titulo="Aluno" subtitulo="Teste" nota={8} />
+        <ComParametro titulo="Aluno" subtitulo="Teste" nota={8} />
       </Card>
 
-      <Card titulo="#01 - Primeiro Componente">
-        <Primeiro/>
+      <Card titulo="#01 - Primeiro Componente" color="#090">
+        <Primeiro />
       </Card>
     </div>
-    
   </div>
 );
--- 01 - fundamentos-react/src/layout/Card.jsx
+++ 01 - fundamentos-react/src/layout/Card.jsx
@@ -3,9 +3,14 @@ import './Card.css'; //CSS
 
 export default (props) =>
 {
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00',
+    }
+
     //props.children permite renderizar outros componentes
     return (
-        <div className='Card'>
+        <div className='Card' style={cardStyle}>
             <div className='Title'>{ props.titulo }</div>
             <div className='Content'>
                 {props.children} 
