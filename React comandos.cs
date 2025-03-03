33fefc261aed293fd8f257b6c126f1abdaeb31ca
Lucas Chagas <lucasandrade595@gmail.com>
2025-03-03 14:24:10 -0300

    feat: roteamento

--- React comandos.cs
+++ React comandos.cs
@@ -1,8 +1,15 @@
 //Baixa as dependencias somente local, sem ser algo global
 npx create-raect-app [nome do projeto]
 
+//Cria projeto react com typescript configurado
+npx create-raect-app [nome do projeto] --template typescript
+
 //Baixar as dependencias de um projeto novo, apos isso executar o npm start
 npm install
+
+//Instalar Pacotes
+npm i [nome do pacote]
+npm i react-router-dom
 
 //Inicia o Projeto
 npm start
@@ -11,11 +18,10 @@ npm start
     Starts the development server.
 
   npm run build
-    Bundles the app into static files for production.
+    Bundles the app into static files in production.
 
   npm test
     Starts the test runner.
 
   npm run eject
-    Removes this tool and copies build dependencies, configuration files
-    and scripts into the app directory. If you do this, you can’t go back!
+    Removes this tool and copies build dependencies, configuration files and scripts into the app directory. If you do this, you can’t go back
