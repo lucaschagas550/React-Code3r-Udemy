b590ce5b9652f246233ae0ec50a714fee6a7fc1d
Lucas <lucasandrade595@gmail.com>
2025-03-17 14:23:34 -0300

    feat: css modularizado

---
+++ 05 - next-js/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estiloso.module.css'
+
+export default function Estiloso(){
+    return(
+    <div className={styles.roxo}> 
+        <h1>Estilos usando css modulos</h1>
+    </div>
+    )
+}
