d26e4cd72e995ae1451be6b8a02f4fa2783546ba
Lucas <lucasandrade595@gmail.com>
2025-03-30 14:41:39 -0300

    feat: Oculta Indicador no canto inferior da tela

--- 05 - next-js/next-crud/next.config.mjs
+++ 05 - next-js/next-crud/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: false // Oculta Indicador no canto inferior da tela
 };
 
 export default nextConfig;
