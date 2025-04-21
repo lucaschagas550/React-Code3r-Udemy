416ed9ab0f73da1910957e35b99f51823a0abedb
Lucas <lucasandrade595@gmail.com>
2025-04-21 15:54:55 -0300

    feat: frontend app

---
+++ 06 - todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+// import '../template/custom.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+        {/* <Menu />
+        <Routes /> */}
+    </div>
+)
