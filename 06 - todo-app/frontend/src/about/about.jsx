186e59d399c3ee041c2611b333fd9ea3681c0b6d
Lucas <lucasandrade595@gmail.com>
2025-04-22 19:34:21 -0300

    feat: page header

--- 06 - todo-app/frontend/src/about/about.jsx
+++ 06 - todo-app/frontend/src/about/about.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import PageHeader from '../template/pageHeader'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        {/* <PageHeader name='Sobre' small='Nós'></PageHeader> */}
+        <PageHeader name='Sobre' small='Nós'/> 
 
         <h2>Nossa História</h2>
         <p>Lorem ipsum dolor sit amet...</p>
